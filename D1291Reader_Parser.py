5bb421f70a66bc0bd467cc765d599e53d42188d8
Shaurya Prakash <bit.curiosity@gmail.com>
2022-06-22 12:19:08 +0530

    Assignment_1 Metaheuristics
    Assignment_1 Metaheuristics

---
+++ D1291Reader_Parser.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 19 15:14:54 2022
+purpose : to view and present the content of d1291.tsp file.
+dependency :tsplib95 , pandas , numpy
+IDE :use spyder and variable explorer for best interpretations.
+@author: shaurya
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 19 15:05:36 2022
+
+@author: shaurya
+"""
+
+import tsplib95
+import pandas as pd
+import numpy as np
+import re
+# to collect values 
+values=[]
+#to collect values to create column1 of dataframe
+val1dict = dict()
+#to collect values to create column2 of dataframe
+val2dict = dict()
+#input file path
+filepath = 'C:/Users/shaurya/Downloads/d1291.tsp'
+problem=''
+with open(filepath) as f:
+    text = f.read()
+    problem = tsplib95.parse(text)
+    def openTSP(filepath) :
+        with open(filepath) as f:
+            text = f.read()
+            problem = tsplib95.parse(text)
+            values=(text.splitlines())
+            return values
+
+    def valueProvider(li):
+        ss=[]
+        for i in range(0,len(li)) :
+             if li[i] != '' :
+                ss.append(float(li[i]))         
+        return ss
+
+
+    #create dataframe from values
+    def dataFrameCreator(values,val1dict,val2dict) :
+        for i in range(0,len(values)) :
+            ss=valueProvider(values[i].strip(' ').split(" "))
+            val1dict[i]=ss[0]
+            val2dict[i]=ss[1]
+        data = {"VALUE1":[val1dict[i] for i in val1dict],
+                "VALUE2":[val1dict[i] for i in val2dict],} 
+        df= pd.DataFrame(data)
+        return df
+
+    values=openTSP(filepath)
+    df=dataFrameCreator(values,val1dict,val2dict)
+    df.to_string('./D1291.txt',index = False)
+    print(df)
---
+++ U724_7_Parser_Reader-.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 19 16:39:19 2022
+
+@author: shaurya
+"""
+
+
+import tsplib95
+import pandas as pd
+import numpy as np
+import re
+filepath = 'C:/Users/shaurya/Downloads/u724-7.csp2'
+problem=''
+values =[]
+val1dict = dict()
+val2dict = dict()
+with open(filepath) as f:
+    text = f.read()
+    problem = tsplib95.parse(text)
+    values=text.split("\n")
+    
+#input : list of string ,output list of integers
+def valueProviderInt(li):
+    ss=[]
+    for i in range(0,len(li)) :
+          if li[i] != '' :
+            ss.append(int(li[i]))         
+    return ss
+
+#input : list of string ,output list of strings
+def valueProviderStr(li):
+    ss=[]
+    for i in range(0,len(li)) :
+          if li[i] != '' :
+            ss.append((li[i]))         
+    return ss
+
+
+
+    #create dataframe from values
+def dataFrameCreator1(values,val1dict) :
+    for i in range(4,727) :
+        ss=valueProviderInt(values[i].strip(' ').split(" "))
+        val1dict[i]=ss
+        data = {"DISTANCE MATRIX":[val1dict[i] for i in val1dict]} 
+        df= pd.DataFrame(data)
+    return df
+df1=dataFrameCreator1(values,val1dict)
+
+def dataFrameCreator2(values,val2dict) :
+    for i in range(729,1452) :
+        ss=valueProviderInt(values[i].strip(' ').split(" "))
+        val2dict[i]=ss
+        data = {"COVERAGE":[val2dict[i] for i in val2dict]} 
+        df= pd.DataFrame(data)
+    return df
+df2=dataFrameCreator2(values,val2dict)
+
+df1.to_string('./u274_7_11_df1.txt',index = False)
+df2.to_string('./u274_7_11_df2.txt',index = False)
